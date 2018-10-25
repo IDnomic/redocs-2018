b2ece8c14784b36128b8f5ff11aa8c68f39dae32
Mathieu Valois <mathieu.valois@unicaen.fr>
2018-10-25 13:06:56 +0200

    Popup now shows fingerprints validation status

--- webextension/ledger-pki.js
+++ webextension/ledger-pki.js
@@ -21,6 +21,9 @@ var extraInfoSpec = ['blocking'];
 
 const serv_location = 'http://127.0.0.1:8080/?action=check&stream=stream1&hash=';
 
+const certificates = Array();
+const checked_fingerprints = Array();
+
 browser.webRequest.onHeadersReceived.addListener(async function(details){
 	var tabId = details.tabId;
 	var requestId = details.requestId;
@@ -30,17 +33,33 @@ browser.webRequest.onHeadersReceived.addListener(async function(details){
 	});
 
 	for(cert_rank in securityInfo.certificates){
-		var certificate = securityInfo.certificates[cert_rank];
-		var sha256_digest = certificate.fingerprint.sha256.replace(/:/g, '').toLowerCase();
+		let certificate = securityInfo.certificates[cert_rank];
+		let sha256_digest = certificate.fingerprint.sha256.replace(/:/g, '').toLowerCase();
 
-		var req = new XMLHttpRequest();
-		var req_location = serv_location + sha256_digest;
+		if (checked_fingerprints.indexOf(certificate.fingerprint.sha256) != -1){
+			continue;
+		}
+
+		certificates.push(certificate);
+		checked_fingerprints.push(certificate.fingerprint.sha256);
+
+
+		let req = new XMLHttpRequest();
+		let req_location = serv_location + sha256_digest;
 		req.open('GET', req_location, true);
 		req.onload = function (e) {
 			if (req.readyState === 4 && req.status === 200) {
 				if (req.responseText == "invalid") {
 					invalidCertificates(tabId);
+					valid = false;
 				}
+				else if (req.responseText == "valid"){
+					valid = true;
+				}
+				cert_idx = certificates.indexOf(certificate);
+				newcert = certificates[cert_idx];
+				newcert.is_bcvalid = valid;
+				certificates[cert_idx] = newcert;
 			}
 		};
 		req.onerror = function (e) {
@@ -49,3 +68,9 @@ browser.webRequest.onHeadersReceived.addListener(async function(details){
 		req.send();
 	}
 }, ALL_SITES, extraInfoSpec)
+
+browser.runtime.onConnect.addListener(function(port){
+    port.onMessage.addListener(function(msg) {
+		port.postMessage(certificates);
+    });
+});
---
+++ webextension/popup/cert_state.js
@@ -0,0 +1,22 @@
+ var body = document.getElementById("body");
+ var port = browser.runtime.connect();
+ port.postMessage();
+ port.onMessage.addListener(function(msg) {
+ 	for (cert_idx in msg){
+		cert = msg[cert_idx];
+		var node = document.getElementsByClassName("cert")[0];
+		var newnode = node.cloneNode(true);
+		newnode.querySelector('.cert_fingerprint').innerHTML = cert.fingerprint.sha256.replace(/:/g, '').toLowerCase();
+		if (cert.is_bcvalid){
+			newnode.classList.add("valid");
+		}
+		else if (! cert.is_bcvalid){
+			newnode.classList.add("invalid");
+		}
+		else {
+			newnode.classList.add("normal");
+		}
+		newnode.style.display = "initial";
+		body.appendChild(newnode);
+	}
+ });
